5e807dac628d9a7925f9a773f89dd78e09d5985d
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-22 17:20:12 -0300

    Configurando as imagens nas funções

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const html = document.querySelector("html");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
+const botaoIniciar = document.getElementById("start-pause");
+const banner = document.querySelector(".app__image");
+
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
 
 focoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "foco");
+    banner.setAttribute("src", "/imagens/foco.png");
 });
 
 curtoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "descanso-curto");
+    banner.setAttribute("src", "/imagens/descanso-curto.png");
 });
 
 longoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "descanso-longo");
+    banner.setAttribute("src", "/imagens/descanso-longo.png");
+
 });
