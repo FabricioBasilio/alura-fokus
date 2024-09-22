fd6f6146b58033898e73db946633b4ab0eaf6b1b
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-22 19:10:15 -0300

    Mudando o estilo "active" de acordo com o botão

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const html = document.querySelector("html");
+const botoes = document.querySelectorAll(".app__card-button");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
@@ -6,24 +7,30 @@ const botaoIniciar = document.getElementById("start-pause");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 
-const duracaoFoco = 1500; 
-const duracaoDescansoCurto = 300; 
-const duracaoDescansoLongo = 900; 
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener("click", () => {
     alterarContexto("foco");
+    focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
     alterarContexto("descanso-curto");
+    curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
     alterarContexto("descanso-longo");
+    longoBt.classList.add("active");
 
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove("active");
+    });
     html.setAttribute("data-contexto", contexto);
     banner.setAttribute("src", `/imagens/${contexto}.png`);
 
