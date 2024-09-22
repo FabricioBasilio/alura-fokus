4556d14b6d08285fc8ac24480069131a179278e9
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-22 17:57:32 -0300

    Modificando o título de acordo com a função

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const botaoIniciar = document.getElementById("start-pause");
 const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
 
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
@@ -25,4 +26,19 @@ longoBt.addEventListener("click", () => {
 function alterarContexto(contexto) {
     html.setAttribute("data-contexto", contexto);
     banner.setAttribute("src", `/imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
 }
